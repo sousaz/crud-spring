dc5063018d4713718006faf0076e68d5115068ae
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-14 21:18:33 -0300

    feat: was created a post method for course controller to create a course

--- src/main/java/com/sousaz/crudspring/controller/CourseController.java
+++ src/main/java/com/sousaz/crudspring/controller/CourseController.java
@@ -2,7 +2,11 @@ package com.sousaz.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,10 @@ public class CourseController {
     @GetMapping // mapeando o metodo como GET
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course){
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
