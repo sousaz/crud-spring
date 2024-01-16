1a08832718b706a85d61b150c2c457ae3f6594fc
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-16 17:04:09 -0300

    feat: was created a get method to get course by id

--- src/main/java/com/sousaz/crudspring/controller/CourseController.java
+++ src/main/java/com/sousaz/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +28,17 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     public ResponseEntity<Course> create(@RequestBody Course course){
         return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
+
+
 }
